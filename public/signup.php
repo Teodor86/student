fc14a99af8d9ade4801ec6ed1c06cc01866fd1ad
Teodor86 <emilhuseynov86@gmail.com>
2017-04-23 18:48:20 +0400

    Update signup.php

--- public/signup.php
+++ public/signup.php
@@ -7,27 +7,27 @@ $generatePass = new PasswordGenerator;
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    $db = new PDOFactory;
-    $pdo = $db->getConnection();
-    $user = new User($_POST);
-    $userGateway = new UserGateway($pdo);
-    $validate = new UserValidator($userGateway);
-    $errors = $validate->check($user);
+	$db = new PDOFactory;
+	$pdo = $db->getConnection();
+	$user = new User($_POST);
+	$userGateway = new UserGateway($pdo);
+	$validate = new UserValidator($userGateway);
+	$errors = $validate->check($user);
 
-		if(empty($errors)) {
+	if(empty($errors)) {
 			
-        $code = $generatePass->createPass(60);
-        $user->setCode($code);
-        Authorization::setAuthorize($user->getCode());
-							
-        $result = $userGateway->save($user);
+		$code = $generatePass->createPass(60);
+		$user->setCode($code);
+		Authorization::setAuthorize($user->getCode());
+
+		$result = $userGateway->save($user);
 		
-        if($result !== false) {
-            $_SESSION['success'] = 'Регистрация прошло успешно';
-            header('Location:edit.php');
-            exit;
-        }			
-		}
+		if($result !== false) {
+			$_SESSION['success'] = 'Регистрация прошло успешно';
+			header('Location:edit.php');
+			exit;
+		}			
+	}
 }
 
 $pageTitle = 'Регистрация пользователя';
