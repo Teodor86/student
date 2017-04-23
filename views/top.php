9c42341a386918433cf40ace57addb254d1b708b
Teodor86 <emilhuseynov86@gmail.com>
2017-04-23 11:18:30 +0400

    Delete top.php

--- views/top.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title><?=htmlspecialchars($pageTitle, ENT_QUOTES);?></title>
-		<link href="../css/main.css" rel="stylesheet">
-	</head>
