abe01d682c727c77c680f65c89dfb077914c01c3
Emiliano1986 <emilhuseynov86@gmail.com>
2021-04-25 11:45:25 +0400

    Класс пользователя.

--- src/Models/Users/User.php
+++ src/Models/Users/User.php
@@ -4,18 +4,37 @@ namespace Models\Users;
 
 class User
 {
+    /** @var string */
     private $name;
+
+    /** @var string */
     private $surname;
+
+    /** @var string */
     private $email;
+
+    /** @var string */
     private $birthday;
+
+    /** @var  string */
     private $gender;
+
+    /** @var string */
     private $code;
+
+    /** @var int */
     private $uid;
 
+    /** @var string */
     const GENDER_MALE = 'M';
+
+    /** @var string */
     const GENDER_FEMALE = 'F';
 
-    public function setAttributes(array $input)
+    /**
+     * @param array $input
+     */
+    public function setAttributes(array $input): void
     {
         foreach ($input as $key => $value) {
             if (property_exists("Models\Users\User", $key)) {
@@ -24,71 +43,113 @@ class User
         }
     }
 
+    /**
+     * @param string $name
+     */
     public function setName(string $name)
     {
         $this->name = $name;
     }
 
+    /**
+     * @return string|null
+     */
     public function getName(): ?string
     {
         return $this->name;
     }
 
+    /**
+     * @param string $surname
+     */
     public function setSurname(string $surname)
     {
         $this->surname = $surname;
     }
 
+    /**
+     * @return string|null
+     */
     public function getSurname(): ?string
     {
         return $this->surname;
     }
 
+    /**
+     * @param string $email
+     */
     public function setEmail(string $email)
     {
         $this->email = $email;
     }
 
+    /**
+     * @return string|null
+     */
     public function getEmail(): ?string
     {
         return $this->email;
     }
 
+    /**
+     * @param $birthday
+     */
     public function setBirthday($birthday)
     {
         $this->birthday = $birthday;
     }
 
-    public function getBirthday()
+    /**
+     * @return string
+     */
+    public function getBirthday(): string
     {
         return $this->birthday;
     }
 
+    /**
+     * @param string $gender
+     */
     public function setGender(string $gender)
     {
         $this->gender = $gender;
     }
 
+    /**
+     * @return string|null
+     */
     public function getGender(): ?string
     {
         return $this->gender;
     }
 
+    /**
+     * @param string $code
+     */
     public function setCode(string $code)
     {
         $this->code = $code;
     }
 
+    /**
+     * @return string|null
+     */
     public function getCode(): ?string
     {
         return $this->code;
     }
 
+    /**
+     * @param int $uid
+     */
     public function setUserId(int $uid)
     {
         $this->uid = $uid;
     }
 
+    /**
+     * @return int|null
+     */
     public function getUserId(): ?int
     {
         return $this->uid;
