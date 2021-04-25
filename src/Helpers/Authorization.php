f8f76762063103f4ced9f464beaa020444de7517
Emiliano1986 <emilhuseynov86@gmail.com>
2021-04-25 11:39:06 +0400

    Авторизация пользователя.

--- src/Helpers/Authorization.php
+++ src/Helpers/Authorization.php
@@ -4,16 +4,23 @@ namespace Helpers;
 
 class Authorization
 {
-    public static function setAuthorize(string $code)
+    /**
+     * @param string $code
+     */
+    public static function setAuthorize(string $code): void
     {
         setcookie('user', $code, time() + 60 * 60 * 24 * 365, '/', null, null, true);
     }
 
-    public static function getCookie()
+    /**
+     * @return string|null
+     */
+    public static function getUserByCookie() :?string
     {
         if (isset($_COOKIE['user'])) {
             return $_COOKIE['user'];
         }
+        return null;
     }
 }
 
