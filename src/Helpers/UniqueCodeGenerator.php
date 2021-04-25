11e6850878fd643a4d4b64074d5aa537e27f156f
Emiliano1986 <emilhuseynov86@gmail.com>
2021-04-25 08:48:50 +0400

    Создает уникальный код.

--- src/Helpers/UniqueCodeGenerator.php
+++ src/Helpers/UniqueCodeGenerator.php
@@ -4,6 +4,10 @@ namespace Helpers;
 
 class UniqueCodeGenerator
 {
+    /**
+     * @param $length
+     * @return string
+     */
     public static function createCode($length)
     {
         $chars = [
