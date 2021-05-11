3e909b7d1e0a7131e88d53279ccad0bc2d3ca710
Emiliano1986 <emilhuseynov86@gmail.com>
2021-05-11 17:30:48 +0400

    Update public

--- public/signup.php
+++ public/signup.php
@@ -3,19 +3,15 @@ ob_start();
 session_start();
 require_once __DIR__ . '/../src/init.php';
 
-$container->register('\Models\Users\UserDbGateway', function (\Services\DIContainer $container) {
-    return new \Models\Users\UserDbGateway($container->get('PDO'));
-});
-
-$userDbGateway = $container->get('\Models\Users\UserDbGateway');
+$userDbGateway = $container->get('\Services\UserDbGateway');
+$authHelper = $container->get('\Services\Authorization');
 
 $user = new Models\Users\User;
 $unique = new Helpers\UniqueCodeGenerator;
-$validate = new Models\Users\UserValidator($userDbGateway);
+$validator = new Services\UserValidator($userDbGateway);
 
-$isUserExists = new Helpers\Authorization($userDbGateway);
-if ($isUserExists->getUserByCookie()) {
-    $user->setAttributes($isUserExists->getUserByCookie());
+if ($authHelper->getUserByToken()) {
+    $user = $authHelper->getUserByToken();
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -28,23 +24,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     $user->setAttributes($input);
-    $errors = $validate->check($user);
+
+    $errors = [];
+    $errors = $validator->check($user);
 
     if (empty($errors)) {
-        if ($isUserExists->getUserByCookie()) {
-            $result = $userDbGateway->update($user);
-            Helpers\FlashMessage::setFlashMessage('editOfUser', 'Ваш профиль редактирирован.', 'success');
+        if ($authHelper->getUserByToken()) {
+            $userDbGateway->update($user);
+            Helpers\FlashMessage::set('editOfUser', 'Ваш профиль редактирирован.', 'success');
 
-            if ($result !== false) {
-                header('Location:index.php');
-                exit;
-            }
+            header('Location:index.php');
+            exit;
+
         } else {
             $user->setCode($unique->createCode(80));
-            Helpers\Authorization::setAuthorize($user->getCode());
-
             $userDbGateway->save($user);
-            Helpers\FlashMessage::setFlashMessage('accountOfUser', 'Поздравляю с успешной регистрацией!', 'success');
+            Services\Authorization::createToken($user);
+
+            Helpers\FlashMessage::set('accountOfUser', 'Поздравляю с успешной регистрацией!', 'success');
 
             header('Location:index.php');
             exit;
