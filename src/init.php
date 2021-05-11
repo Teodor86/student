27da792fe6c69f4889706928a811171be1df9b45
Emiliano1986 <emilhuseynov86@gmail.com>
2021-05-11 17:20:35 +0400

    Update init.php

--- src/init.php
+++ src/init.php
@@ -9,7 +9,15 @@ spl_autoload_register(function ($className) {
     }
 });
 
-set_exception_handler(function ($exception) {
+set_error_handler(function ($errno, $errmsg, $filename, $linenum) {
+    if (!error_reporting()) {
+        return;
+    }
+
+    throw new ErrorException($errmsg, $errno, 0, $filename, $linenum);
+});
+
+set_exception_handler(function (Throwable $exception) {
     header('HTTP/1.1 503 Service Temporarily Unavailable');
     header('Status: 503 Service Temporarily Unavailable');
     error_log($exception->__toString(), 3, '../src/error.log');
@@ -20,15 +28,14 @@ set_exception_handler(function ($exception) {
 $container = new \Services\DIContainer;
 
 $container->register('config', function (\Services\DIContainer $container) {
-    $config = __DIR__ . '/config.php';
+    $config = (require __DIR__ . '/config.php')['db'];
     return $config;
 });
 
 $container->register('PDO', function (\Services\DIContainer $container) {
-    // Для соединения с БД нам нужны данные из конфига, получаем их
-    // из контейнера
-    require $config = $container->get('config');
-    $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8";
+    $config = $container->get('config');
+
+    $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8mb4";
     $pdo = new \PDO($dsn, $config['user'], $config['pass'], [
         \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
         \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
@@ -36,4 +43,10 @@ $container->register('PDO', function (\Services\DIContainer $container) {
     return $pdo;
 });
 
-?>
+$container->register('\Services\UserDbGateway', function (\Services\DIContainer $container) {
+    return new \Services\UserDbGateway($container->get('PDO'));
+});
+
+$container->register('\Services\Authorization', function (\Services\DIContainer $container) {
+    return new \Services\Authorization($container->get('\Services\UserDbGateway'));
+});
